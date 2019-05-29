0abb595886384f66a8de7107c72761a6609386a9
anduser <huraccane@gmail.com>
2019-05-29 11:56:04 +0300

    Операции

---
+++ Autojava/src/Autojava.java
@@ -0,0 +1,23 @@
+public class Autojava {
+
+    public static void main(String[] args) {
+        System.out.println("Hello,World!");
+        // первый комментарий
+        System.out.println("Первый");
+        System.out.println("1123");
+
+        System.out.println(2 + 2);
+        System.out.println(2 * 2);
+        System.out.println(2 / 2);
+        System.out.println(2 - 2);
+
+        System.out.println(1 / 2);
+        System.out.println(1.0 / 2);
+        System.out.println("2" + "2");
+        System.out.println("2" + 2);
+
+        System.out.println("2 + 2 = " + 2 + 2);
+        // write your code here
+    }
+}
+
