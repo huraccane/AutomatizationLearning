7dab53c63ab0a14333c34c6ab2e85a7d9a754e6b
anduser <huraccane@gmail.com>
2019-05-30 15:43:09 +0300

    Проверка связи 3.0

--- Autojava/src/Autojava.java
+++ Autojava/src/Autojava.java
@@ -2,6 +2,7 @@ public class Autojava {
 
     public static void main(String[] args) {
         System.out.println("Hello,World!");
+        System.out.println("Hello," + "World!");
         // первый комментарий
         System.out.println("Первый");
         System.out.println("1123");
@@ -19,6 +20,14 @@ public class Autojava {
 
         System.out.println("2 + 2 = " + 2 + 2);
         System.out.println((2 + 2 + 2 + (2 / 2)) / 0.5);
+
+        //int l = 8;
+        double l = 8.0;
+        double s = l * l;
+        String somebody = "Площадь квадрата со стороной! ";
+
+        System.out.println(somebody + l + " = " + s);
+
         // write your code here
     }
 }
